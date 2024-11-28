c39bdb18cadf1bf4b23644cf2c54ae75c95b0211
FredericoRosa <fredcoutorosa@gmail.com>
2024-11-28 10:48:54 -0300

    Alteração do Arquivo Instrução e Inclusão do Arquivo com Ex01

---
+++ Formacao_Github_Certification/resolvendo-codigos-py-copilot/concat_dados.py
@@ -0,0 +1,17 @@
+# Descrição: Vamos receber dois dados diferentes do usuário e concatená-los em uma única string
+
+# Recebe os dados do usuário
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# Concatena os dados
+resultado = dado1 + dado2
+
+# Exibe o resultado
+print("Resultado da concatenação:", resultado)
+
+# Concatena os dados com separação
+resultado_com_separacao = dado1 + " " + dado2
+
+# Exibe o resultado com separação
+print("Resultado da concatenação com separação:", resultado_com_separacao)
